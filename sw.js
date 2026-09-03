58c3134cad807d734e1bceb0163af3b6d9312817
htcongsgn <htcongsgn@gmail.com>
2026-09-03 21:49:13 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='game-cau-ca-v3-1';
-const FILES=['./','./index.html','./manifest.webmanifest','./assets/poca.png','./assets/pica.png','./assets/icon-192.png','./assets/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
